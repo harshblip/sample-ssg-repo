99d894ef28aeb6a43cac4547f30256a888b1fa25
YashJipkate <yashjipkate@gmail.com>
2020-04-16 22:48:34 +0530

    Update script

--- clone.sh
+++ clone.sh
@@ -3,6 +3,6 @@
 git submodule add https://github.com/YashJipkate/sample-content-repo
 
 mv sample-content-repo/* content/
-git add . && git commit -m \"Commit\"
+git add . && git commit -m Update content
 
 rm -rf sample-content-repo
