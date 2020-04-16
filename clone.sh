ea4e3301d180a9e240273cf3dc9ad4d905b68a0a
YashJipkate <yashjipkate@gmail.com>
2020-04-16 22:18:37 +0530

    Add commit command to script

--- clone.sh
+++ clone.sh
@@ -5,5 +5,6 @@ rm -rf content/*
 git clone https://github.com/YashJipkate/sample-content-repo
 
 mv sample-content-repo/* content/
+git add . && git commit -m \"Commit\"
 
 rm -rf sample-content-repo
