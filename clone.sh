9aad0a501fcfc84153de3a6a0b97b47aa7ed20f3
YashJipkate <yashjipkate@gmail.com>
2020-04-16 22:43:48 +0530

    "Commit"

--- clone.sh
+++ clone.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-rm -rf content/*
-
-git clone https://github.com/YashJipkate/sample-content-repo
+git submodule add https://github.com/YashJipkate/sample-content-repo
 
 mv sample-content-repo/* content/
 git add . && git commit -m \"Commit\"
